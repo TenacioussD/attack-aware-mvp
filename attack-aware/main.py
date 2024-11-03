a9bfa18a38bd4165ddfb33e702b8562ff35dfe31
TenaciousD <121198103+TenacioussD@users.noreply.github.com>
2024-11-03 14:04:43 +1300

    Start of working application

--- attack-aware/main.py
+++ attack-aware/main.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)    # Initialises the application            
 
-@app.route("/")    # Route for home page URL
+@app.route('/')    # Route for home page URL
 def home():
-    return render_template("index.html") # Renders the HTML file from templates
+    return render_template('home.html') # Renders the HTML file from templates
 
 if __name__ == "__main__":
     app.run(debug=True)  # Enables debug mode
